d5fa72529b9369973fb917fcc176d2e9106337d2
FirdavsNurmatov <nurmatovfirdavs96@gmail.com>
2025-02-12 13:24:33 +0500

    updated

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 const form = document.querySelector(".form");
-const inputs = document.querySelectorAll(".inputs");
+const input = document.querySelector(".input");
 const box = document.querySelector(".box");
+import { getLocalData, setData } from "./storage.js";
 
-let users = [];
-
-const render = () => {
-  box.innerHTML = users
-    .map(
+const render = (data) => {
+  box.innerHTML = data
+    ?.map(
       (item) =>
         `<div class="item">
-            <h1>${item.user_name}</h1>
-            <h1>${item.user_lastname}</h1>
-            <button id="${item?.id}" class="edit__btn">edit</button>
-            <button id="${item?.id}" class="delete__btn">delete</button>
-        </div>`
+            <h1>${item.title}</h1>
+            <button id="${item.id}" class="edit__btn">edit</button>
+            <button id="${item.id}" class="delete__btn">delete</button>
+            <button id="${item.id}" class="save_data">save</button>
+            </div>`
     )
     .join("");
 };
@@ -32,32 +31,115 @@ const render = () => {
 
 // getData();  console.log(users);
 
-form.addEventListener("submit", (e) => {
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   let obj = {};
+
+//   for (let i of inputs) {
+//     obj[i.name] = i.value;
+//     i.value = "";
+//   }
+
+//   users.push({ ...obj, id: Date.now() });
+//   render();
+// });
+
+// box.addEventListener("click", (e) => {
+//   if (e.target.className === "delete__btn") {
+//     users = users.filter((item) => item.id != e.target.id);
+//   } else if (e.target.className === "edit__btn") {
+//     const userName = prompt("Name:");
+//     const userLastName = prompt("Last name:");
+//     users.map((item) => {
+//       if (item.id == e.target.id) {
+//         item.user_name = userName;
+//         item.user_lastname = userLastName;
+//       }
+//     });
+//   }
+//   render();
+// });
+
+const getData = async () => {
+  try {
+    const res = await fetch("http://localhost:3600/todos");
+    const data = await res.json();
+
+    render(data);
+  } catch (error) {
+    console.log("getdata");
+  }
+};
+
+getData();
+
+const getSingleData = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3600/todos/${id}`);
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    console.log("getsingledata");
+  }
+};
+
+const deleteData = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3600/todos/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return res;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  let obj = {};
-
-  for (let i of inputs) {
-    obj[i.name] = i.value;
-    i.value = "";
+  try {
+    const res = await fetch("http://localhost:3600/todos", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ title: input.value }),
+    });
+    input.value = "";
+    const result = await res.json();
+    getData();
+  } catch (error) {
+    console.log("addeventlistener form");
   }
-
-  users.push({ ...obj, id: Date.now() });
-  render();
 });
 
-box.addEventListener("click", (e) => {
-  if (e.target.className === "delete__btn") {
-    users = users.filter((item) => item.id != e.target.id);
-  } else if (e.target.className === "edit__btn") {
-    const userName = prompt("Name:");
-    const userLastName = prompt("Last name:");
-    users.map((item) => {
-      if (item.id == e.target.id) {
-        item.user_name = userName;
-        item.user_lastname = userLastName;
+box.addEventListener("click", async (e) => {
+  if (e.target.className === "save_data") {
+    const data = await getSingleData(e.target.id);
+    if (data) {
+      const oldData = getLocalData("users") || [];
+
+      const singleItem = oldData.find((item) => item.id == e.target.id);
+      if (!singleItem) {
+        setData("users", [...oldData, data]);
       }
-    });
+    }
+  } else if (e.target.className === "delete__btn") {
+    const data = await getSingleData(e.target.id);
+    console.log(e.target.id);
+    if (data) {
+      const oldData = getLocalData("users") || [];
+
+      const newData = oldData.filter((item) => item.id != e.target.id);
+      setData("users", [...newData]);
+
+      await deleteData();
+    }
   }
-  render();
 });
